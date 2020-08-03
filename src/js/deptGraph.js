8ed362818ebca4d10d3503bbfbdf330dda53bcf6
pdhyani <pdhyani@umich.edu>
2020-08-02 22:08:54 -0500

    init

--- src/js/deptGraph.js
+++ src/js/deptGraph.js
@@ -337,6 +337,18 @@ class Search {
       .attr("height", (d) => this.height - this.y(d.value))
       .attr("fill", "#374567");
 
+    function getcolor(mode){
+      if(mode === "Online"){
+        return "#d95f02"
+      }
+      else if(mode === "Hybrid"){
+        return "#7570b3"
+      }
+      else{
+        return "#1b9e77"
+      }
+    }
+
     const update = function (search) {
       // const data = rawdata;
       let data = rawdata.filter(function (d) {
@@ -357,7 +369,7 @@ class Search {
         .attr("class", "mybar")
         .attr("width", this.x.bandwidth())
         .attr("height", (d) => this.height - this.y(d.value))
-        .attr("fill", "#374567");
+        .attr("fill", function(d){ return getcolor(d.key)});
     }.bind(this);
 
     onFilter();
